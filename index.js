a8c8c521b6b105d81eae4ef5d74d12c03e0d46c7
ChristoherHenao <cq.henao@gmail.com>
2021-04-13 21:40:08 -0400

    finished all 19 questions

--- index.js
+++ index.js
@@ -38,7 +38,12 @@ Do the following:
 
    HINT: no function required
 */
+let a= 4;
+let b= 3;
 
+if(0<10){
+  console.log(a*b);
+}
 
 /*
 Task 1c - Convert Strings to Numbers
@@ -62,7 +67,6 @@ Do the following:
    2. Receive the parameters: a and b
    3. Multiply a and b and return the answer
 */
-
 function multiply(a,b){
     return a*b;
   }
@@ -79,7 +83,6 @@ Do the following:
    2. Use the received value to calculate the age in dog years (1 human year is equal to 7 dog years)
    3. Return the newly calculated age
 */
-
 function dogYears(age){
   let dogAge= age*7;
   return dogAge;
@@ -115,8 +118,6 @@ Use the hungryDog function and feeding requirements below to do the following:
 */  
 // Define whether dog is adult or puppy
 // figure out amount of raw food in pounds
-
-
   function hungryDog(weight,age){
     if (age >= 1){
       if(weight <= 5){
@@ -168,10 +169,34 @@ Use the game function below to do the following:
   
   HINT: Remember that the order in which we pass in our arguments matters when it comes to parameters
 */
-var randomNumber= Math.round(Math.random())
+var computer= Math.random()
+
+if(computer <= 0.34){
+  computer="rock";
+}
+else if(computer <= 0.67){
+  computer="paper";
+}
+else if(computer > 0.67){
+  computer="scissors";
+}
 
 function game(user, computer){
-    /*add your code here*/
+  if (user===computer){
+    return "it's a tie";
+  } 
+  else if(user === "rock" && computer === "scissors"){
+    return "you win!";
+  }
+  else if(user==="paper" && computer==="rock"){
+    return "you win!";
+  }
+  else if(user==="scissors" && computer==="paper"){
+    return "you win!";
+  }
+  else{
+    return "you lose!";
+  }
 }
   
   
@@ -216,10 +241,13 @@ Using the annoyingSong function below do the following:
   2. At each invocation, it should RETURN this string (note: the tests are expecting the same string as below):
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
+function annoyingSong(startingNumber){
+  for(let i=startingNumber; i > 0; i--){
+    return `${startingNumber} bottles of soda on the wall, ${startingNumber} bottles of soda, take one down pass it around ${startingNumber - 1} bottles of soda on the wall`;
 
-function annoyingSong(/*add your code here*/){
-        /*add your code here*/
   }
+        
+}
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -237,9 +265,20 @@ Using the grade function below do the following:
    below should return 'you got an F'
 */
   
-function grade(/*Your Code here */){
+function grade(score){
   /*Your Code here */
+  if(score < 60){
+    return 'you got an F';
+  } else if(score <= 69){
+    return 'you got a D';
+  } else if(score <= 79){;
+  return 'you got a C';
+  } else if(score <= 89){
+    return 'you got a B';
+  } else if(score <= 100){
+    return 'you got an A'
   }
+}
   
   
 
